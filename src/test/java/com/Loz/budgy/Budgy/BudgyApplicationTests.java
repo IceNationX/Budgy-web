80d1652ad6bdd41142e2f3d85b2f9f0dcf692399
Mohammad Al-Lozy <157818890+IceNationX@users.noreply.github.com>
2025-08-13 21:42:27 -0400

    Initial Spring Boot scaffold

---
+++ src/test/java/com/Loz/budgy/Budgy/BudgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Loz.budgy.Budgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
