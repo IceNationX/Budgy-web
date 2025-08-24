a235bd356e3d6c8f8cd20c1433ba12f07f2ec128
Mohammad Al-Lozy <157818890+IceNationX@users.noreply.github.com>
2025-08-23 20:24:37 -0400

    Day 2: Expense entity/repo/controller + POST/GET endpoints

--- src/main/java/com/Budgy/BudgyApplication.java
+++ src/main/java/com/Budgy/BudgyApplication.java
@@ -1,13 +1,11 @@
-package com.Loz.budgy.Budgy;
+package com.budgy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BudgyApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BudgyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BudgyApplication.class, args);
+    }
 }
